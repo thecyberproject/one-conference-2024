6b0151e02a9f54c1a1042b29c9f4144270c74e3e
jp <jan-paul.konijn@tno.nl>
2024-08-07 14:51:53 +0200

    added wazuh ubuntu server + storage variable

--- terraform/one/main.tf
+++ terraform/one/main.tf
@@ -1,3 +1,50 @@
+module "wazuh" {
+  count  = 1
+  source = "../base-spec-cloudinit"
+  vm_name        = "ONEC-Wazuh"
+  vm_description = "Wazuh for the one-conference"
+  vm_id               = 1302
+  template_clone      = "ubuntu-server-22.04-cloud-init-template-csthv04"
+  template_full_clone = true
+  admin_username = "ansible"
+  admin_password = "vErYSecureOneConf"
+  ssh_empty      = false
+
+  default_ci_cdrom_storage = var.proxmox_storage
+
+  proxmox_node = "csthv04"
+  primary_network_bridge = "vlan131"
+
+  networks = [
+    {
+      bridge = "vlan131"
+    },
+  ]
+
+  primary_network_cidr_address = "192.168.1.230/24"
+  primary_network_gateway      = "192.168.1.1"
+
+  cores  = 4
+  memory = 4096
+
+  disks = [
+    {
+      virtio0 = {
+        storage = var.proxmox_storage
+        size    = 30
+        format  = "raw"
+      }
+    }
+  ]
+
+  tags = [
+    "wazuh",
+    "ONE-Conference",
+  ]
+}
+
+
+
 module "ad" {
   count = 1
 
@@ -5,7 +52,7 @@ module "ad" {
   vm_name        = "ONEC-Windows-Server-2019-AD"
   vm_description = "Windows AD Server"
 
-  vm_id                  = 630
+  vm_id                  = 1303
   template_clone         = "Windows-Server-2019-ga-template-csthv04"
   template_full_clone    = true
   proxmox_node           = var.proxmox_node
@@ -19,7 +66,7 @@ module "ad" {
   disks = [
     {
       virtio0 = {
-        storage = "nvme"
+        storage = var.proxmox_storage
         size    = 50
         format  = "raw"
       }
@@ -35,7 +82,7 @@ module "client" {
   vm_name        = format("ONEC-Windows-10-%03d", count.index + 1)
   vm_description = "ONEC-Windows netwerk server"
 
-  vm_id                  = 640 + count.index + 1
+  vm_id                  = 1304 + count.index + 1
   template_clone         = "Windows-10-22H2-ga-template-csthv04"
   template_full_clone    = true
   proxmox_node           = var.proxmox_node
@@ -49,7 +96,7 @@ module "client" {
   disks = [
     {
       virtio0 = {
-        storage = "nvme"
+        storage = var.proxmox_storage
         size    = 50
         format  = "raw"
       }
